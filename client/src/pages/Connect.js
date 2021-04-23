7138df805e5275c5cb1e39344e18cdf56483d5d2
mokulling <72229978+mokulling@users.noreply.github.com>
2021-04-22 19:38:33 -0500

    added styles to chat

--- client/src/pages/Connect.js
+++ client/src/pages/Connect.js
@@ -81,15 +81,16 @@ const Connect = ({ children }) => {
                 <Card key={posts._id} className="main">
                   <Card.Body className="create">Post created by: {posts.email}</Card.Body>
 
-                  <Card.Body className="create" date={posts.date}>{posts.date}</Card.Body>
+                  <Card.Body className="create" date={posts.date}>Date Added: {posts.date} <hr /></Card.Body>
                   <Card.Body  text={posts.text}>{posts.text}</Card.Body>
                   {/* <Card.Body description={posts.description}>{posts.description}</Card.Body> */}
 
                   <Chat postId={posts._id} />
-
+                  <div className="msg">Chat history</div>
                   {posts.chats.map(chat => {
                     return (
-                      <ul>{chat}</ul>
+                      <div className="bg">
+                      <ul className="msg">{chat}</ul></div>
                     )
                   })}
                   {posts.firebaseId === currentUser.uid &&
@@ -113,19 +114,26 @@ const Connect = ({ children }) => {
                 <Card key={donations._id} className="main">
                   <Card.Body className="create">Donation posted by: {donations.email}</Card.Body>
 
-                  <Card.Body className="create" date={donations.date}>{donations.date}</Card.Body>
+                  <Card.Body className="create" date={donations.date}>Date Added: {donations.date}<hr /></Card.Body>
                   <Card.Body  className="card-title" title={donations.title}>{donations.title}</Card.Body>
-                  <Card.Body description={donations.description}>{donations.description}</Card.Body>
+                  <Card.Body description={donations.description}>
+                    Item description: {donations.description}</Card.Body>
 
                   {donations.firebaseId === currentUser.uid &&
                     <BsTrashFill name="id"
                       onClick={() => handleDeleteDonationClick(donations._id)}
                       style={{ width: "20px", height: "20px", marginTop: "5px", marginRight: "10px" }} />}
-                  <Chat donationId={donations._id} />
+                  <Chat className="bg" donationId={donations._id} />
+                  <div className="msg">Chat history</div>
 
                   {donations.chats.map(chat => {
                     return (
+                     
+                      <div className="bg">
+                        
                       <ul className="msg">{chat}</ul>
+                      </div>
+                      
                     )
                   })}
                 </Card>
